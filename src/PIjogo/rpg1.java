252a45f2beb639a4a4357644ec437c3e7b38bff7
Giulia Alciati <giulia.alciati@hotmail.com>
2024-10-24 18:01:17 -0300

    Primeiro desafio - operacionais

--- src/PIjogo/rpg1.java
+++ src/PIjogo/rpg1.java
@@ -1,27 +1,37 @@
 package PIjogo;
 
+
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 
 public class rpg1 {
 
- JFrame janela, janelastart, janelacomojogar, janelacreditos;//janela
+ JFrame janela, janelastart, janelacomojogar, janelacreditos, janeladesafio1, pjanela;//janela
  Container con;//conteúdo
- JPanel fundotituloinicial, fundobstart, fundobcomojogar, fundobcreditos, fundobsair, fundojstart, fundojcomojogar, fundojcreditos;//painéis
- JLabel tituloinicial, tstart, tcomojogar, tcreditos;//textos
- JButton bstart, bcomojogar, bcreditos, bsair, bvoltar;//botões
+ JPanel fundotituloinicial, fundomenu, fundojstart, fundojcomojogar, fundojcreditos, fundodesafio1, fundoalternativasd1;//painéis
+ JLabel tituloinicial, tstart, tcomojogar, tcreditos, tdesafio1;//textos
+ JButton bstart, bcomojogar, bcreditos, bsair, bvoltar, alternativaadesafio1, alternativabdesafio1, alternativacdesafio1, alternativaddesafio1;//botões
  Font fontetitulo = new Font("Times New Roman", Font.PLAIN, 40);//fonte
  Font fontecorpo = new Font ("Times New Roman", Font.PLAIN, 30);//fonte
+ Font fontedesafios = new Font("Times New ROman", Font.PLAIN, 20);
 
 public static void main(String[] args) {
     new rpg1();
@@ -48,11 +58,13 @@ public rpg1(){
    tituloinicial.setForeground(Color.white);//cor
    tituloinicial.setFont(fontetitulo);//fonte
 
-   //Botão Start
-   fundobstart = new JPanel();
-   fundobstart.setBounds(405,350,150,50);
-   fundobstart.setBackground(Color.black);
+   fundomenu = new JPanel();
+   fundomenu.setBounds(335, 350, 300, 200);
+   fundomenu.setBackground(Color.black);
+   fundomenu.setLayout(new GridLayout(4,1));
+   con.add(fundomenu);
 
+   //Botão Start
    bstart = new JButton("START");
    bstart.setBackground(Color.black);
    bstart.setForeground(Color.white);
@@ -86,10 +98,6 @@ public rpg1(){
     });
 
     //botão Como Jogar
-    fundobcomojogar = new JPanel();
-    fundobcomojogar.setBounds(380,400,200,50);
-    fundobcomojogar.setBackground(Color.black);
-
     bcomojogar = new JButton("COMO JOGAR");
     bcomojogar.setBackground(Color.black);
     bcomojogar.setForeground(Color.white);
@@ -123,9 +131,6 @@ public rpg1(){
     });
 
      //Botão créditos
-     fundobcreditos = new JPanel();
-     fundobcreditos.setBounds(385,450,200,50);
-     fundobcreditos.setBackground(Color.black);
 
      bcreditos = new JButton("CRÉDITOS");
      bcreditos.setBackground(Color.black);
@@ -158,9 +163,6 @@ public rpg1(){
 
 
      //Botão sair
-     fundobsair = new JPanel();
-     fundobsair.setBounds(383,500,200,50);
-     fundobsair.setBackground(Color.black);
 
      bsair = new JButton("SAIR");
      bsair.setBackground(Color.black);
@@ -187,18 +189,14 @@ public rpg1(){
          public void mouseReleased(MouseEvent e) {}
      });
 
-   fundobstart.add(bstart);//Para adicionar o botão ao painel
-   fundobcomojogar.add(bcomojogar);
-   fundobcreditos.add(bcreditos);
-   fundobsair.add(bsair);
+   fundomenu.add(bstart);//Para adicionar o botão ao painel
+   fundomenu.add(bcomojogar);
+   fundomenu.add(bcreditos);
+   fundomenu.add(bsair);
    fundotituloinicial.add(tituloinicial);//Para adicionar o texto ao painel
    con.add(fundotituloinicial);//Adiciona o painel
-   con.add(fundobstart);//Adiciona o painel
-   con.add(fundobcomojogar);
-   con.add(fundobcreditos);
-   con.add(fundobsair);
     }
-
+      //método para criar um botão "voltar" nas janelas secundárias
     public JButton botaovoltar(JFrame janelaatual) {
         bvoltar = new JButton("VOLTAR");
         bvoltar.setFont(fontecorpo);
@@ -232,6 +230,14 @@ public rpg1(){
         fundojstart = new JPanel();
         fundojstart.setBounds(50, 50, 900, 600);
         fundojstart.setBackground(Color.black);
+        InputMap mj = fundojstart.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap mj1 = fundojstart.getActionMap();
+        mj.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enterPressed");
+        mj1.put("enterPressed", new AbstractAction() {
+    public void actionPerformed(ActionEvent e) {
+        abrirdesafio1();
+    }
+});
 
         tstart = new JLabel("Jogo começa...");
         tstart.setForeground(Color.white);
@@ -239,10 +245,112 @@ public rpg1(){
         fundojstart.add(tstart);
         janelastart.setVisible(true);
         con.add(fundojstart);
+        
     }
+    
+    public void abrirdesafio1() {
+    abdesafio1(); // Abre a janela do desafio 1
+    janelastart.dispose(); // Fecha a janela atual
+}
+    public void abdesafio1(){
+        janeladesafio1 = new JFrame("Desafio 1");
+        janeladesafio1.setSize(1000, 700);
+        janeladesafio1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janeladesafio1.setResizable(false);
+        janeladesafio1.getContentPane().setBackground(Color.black);
+        janeladesafio1.setLocationRelativeTo(null);
+        janeladesafio1.setLayout(null);
+        con = janeladesafio1.getContentPane();
 
+        fundodesafio1 = new JPanel();
+        fundodesafio1.setBounds(50, 50, 900, 600);
+        fundodesafio1.setBackground(Color.black);
+        InputMap mj2 = fundodesafio1.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap mj3 = fundodesafio1.getActionMap();
+        mj2.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enterPressed");
+        mj3.put("enterPressed", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                abrirproximajanela();
+            }
+        });
 
-    public void abcomojogar() { // Método que abre a nova janela com informações sobre "Como Jogar"
+        tdesafio1 = new JLabel("<html>Após o encontro com o feiticeiro, Kote vai até a biblioteca do palácio, mas o livro que ele precisa<br> encontra-se em uma área restrita que contém os seguintes requisitos:<br><br>" + //
+            "<i> if (!(energia < 5 || foco <= 5) && (habilidadeEspecial || sabedoria > 10)) {</i><br>" + 
+            "<i>acessarBiblioteca();</i><br><br>" + 
+            "Considere os valores para Kote:<br>" + 
+            "Energia: 4<br>Foco: 6<br>Habilidade Especial: desativada<br>Sabedoria: 12<br><br>" + 
+            "O que acontece quando Kote tenta acessar a biblioteca?</html>");
+        tdesafio1.setForeground(Color.white);
+        tdesafio1.setFont(fontedesafios);
+
+        fundoalternativasd1 = new JPanel();
+        fundoalternativasd1.setBounds(100, 380, 800, 100);
+        fundoalternativasd1.setBackground(Color.black);
+        fundoalternativasd1.setLayout( new GridLayout(2,2,10,10));
+        con.add(fundoalternativasd1);
+
+        alternativaadesafio1 = new JButton("A) A biblioteca é acessada.");
+        alternativaadesafio1.setFont(fontedesafios);
+        alternativaadesafio1.setFocusPainted(false);
+        alternativaadesafio1.setBackground(Color.black);
+        alternativaadesafio1.setForeground(Color.white);
+        alternativaadesafio1.setContentAreaFilled(false);
+        alternativaadesafio1.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+            JOptionPane.showMessageDialog(null, "Resposta incorreta! Tente novamente!");
+        }
+
+    });
+        alternativabdesafio1 = new JButton("B) A biblioteca permanece fechada.");
+        alternativabdesafio1.setFont(fontedesafios);
+        alternativabdesafio1.setFocusPainted(false);
+        alternativabdesafio1.setBackground(Color.black);
+        alternativabdesafio1.setForeground(Color.white);
+        alternativabdesafio1.setContentAreaFilled(false);
+        
+        alternativabdesafio1.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "Correto! A biblioteca permanece fechada.");
+            abstart();
+            janeladesafio1.dispose();
+            
+    }
+        
+});
+        alternativacdesafio1 = new JButton("C) Kote encontra o livro.");
+        alternativacdesafio1.setFont(fontedesafios);
+        alternativacdesafio1.setFocusPainted(false);
+        alternativacdesafio1.setBackground(Color.black);
+        alternativacdesafio1.setForeground(Color.white);
+        alternativacdesafio1.setContentAreaFilled(false);
+        alternativacdesafio1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Resposta incorreta! Tente novamente!");
+    }
+ });
+        alternativaddesafio1 = new JButton("D) Kote é banido da biblioteca.");
+        alternativaddesafio1.setFont(fontedesafios);
+        alternativaddesafio1.setFocusPainted(false);
+        alternativaddesafio1.setBackground(Color.black);
+        alternativaddesafio1.setForeground(Color.white);
+        alternativaddesafio1.setContentAreaFilled(false);
+        alternativaddesafio1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog( null, "Resposta incorreta! Tente novamente!");
+    }
+    
+ });
+        
+        fundodesafio1.add(tdesafio1);
+        fundoalternativasd1.add(alternativaadesafio1);
+        fundoalternativasd1.add(alternativabdesafio1);
+        fundoalternativasd1.add(alternativacdesafio1);
+        fundoalternativasd1.add(alternativaddesafio1);
+        con.add(fundodesafio1);
+        janeladesafio1.setVisible(true);
+    }    
+
+    public void abcomojogar() { // Método que abre a nova janela 
         // Criando uma nova janela
         janelacomojogar = new JFrame("Como Jogar");
         janelacomojogar.setSize(1000, 700);
@@ -257,7 +365,7 @@ public rpg1(){
         fundojcomojogar.setBounds(50, 50, 900, 600);
         fundojcomojogar.setBackground(Color.black);
         
-        tcomojogar = new JLabel("<html>Esse é um jogo baseado em perguntas de múltipla escolha <br> no qual haverá uma pergunta relacionada a java...<html>");
+        tcomojogar = new JLabel("<html>Esse é um jogo baseado em perguntas de múltipla escolha no qual <br> haverá uma pergunta relacionada a java...<html>");
         tcomojogar.setForeground(Color.white);
         tcomojogar.setFont(fontecorpo);
         fundojcomojogar.add(tcomojogar);
